0c99a7458f6967b5cc25da40fa3bfa7e618ad9c9
Melvin Shieh <shieh.melvin@gmail.com>
2024-11-20 00:51:46 +0000

    21 working version

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -3,6 +3,7 @@ Problem: Create the game of Twenty-One:
 - Rules are like Blackjack 
 - Except no splitting or doubling down 
 - No betting in first iteration 
+- Make the game single player for now, but optional to expand
 
 Pseudocode: 
 1. Initialize deck
@@ -10,22 +11,17 @@ Pseudocode:
 3. Player turn: hit or stay
     - Ask player to hit or stay.
     - If stay, stop asking.
-    - Repeat until bust or stay.
+    - Repeat until bust, 21, or stay.
 4. If player bust, dealer wins.
 5. Dealer turn: hit or stay
    - Repeat until total >= 17
 6. If dealer busts, player wins.
 7. Compare cards and declare winner.
 
-Notes:
-- 
-
 Data Structure: 
 - We could use a dictionary, a list of lists or tuples, or a list 
 - I like the idea of a list of lists or tuples, since we can use 0 or 1 index 
 - Tuples make more sense since the cards can't be changed 
-- We also need a dictionary for values too, 
-
 """
 
 """
@@ -33,27 +29,9 @@ Import needed modules
 Initialize the deck and global variables
 """
 
+# Import modules:
 import random
-
-# Initialize the deck here:
-suites = ['D', 'H', 'C', 'S']
-cards = [num for num in range(2, 11)] + ['J', 'Q', 'K', 'A']
-
-deck = list()
-for suite in suites:
-    for card in cards:
-        deck.append(tuple((suite, card)))
-
-HIDDEN_CARD = ('X', 'X')
-
-# Initialize hands here 
-# Can turn players into objects later that way we can initialize multiple 
-players = {
-    'dealer': [],
-    'player': []
-}
-
-current_player = 'player'
+import pdb
 
 """
 Define functions here
@@ -65,21 +43,23 @@ def prompt(message):
     print(f'--> {message}')
 
 def display_hands(players):
+    prompt(f"Dealer: {players['Dealer'][0]} {HIDDEN_CARD * (len(players['Dealer']) - 1)}")
+    
     for player in players:
-        if player == 'dealer':
-            prompt(f'Dealer: {players[player][0]} {HIDDEN_CARD * (len(players[player]) - 1)}')
-        else:
+        if player != 'Dealer':
+            prompt(f'{player} Total Value: {total_value(players[player])}')
             prompt(f'{player}: {players[player]}')
 
 def display_full_hands(players):
     for player in players:
+        prompt(f'{player} Total Value: {total_value(players[player])}')
         prompt(f'{player}: {players[player]}')
 
-def alternate_player(current_player):
-    if current_player == 'player':
-        return 'dealer'
-    elif current_player == 'dealer':
-        return 'player'
+# def alternate_player(current_player):
+#     if current_player == 'Player':
+#         return 'Dealer'
+#     elif current_player == 'Dealer':
+#         return 'Player'
 
 def deal(players, deck):
     for player in players:
@@ -90,6 +70,12 @@ def hit(current_player, deck):
     card = random.choice(deck)
     players[current_player].append(card)
     deck.remove(card)
+
+def bust(cards):
+    return total_value(cards) > 21
+
+def twenty_one(cards):
+    return total_value(cards) == 21
 
 def total_value(cards):
     values = [card[1] for card in cards]
@@ -110,12 +96,6 @@ def total_value(cards):
     
     return total_value
 
-def bust(cards):
-    return total_value(cards) > 21
-
-def twenty_one(cards):
-    return total_value(cards) == 21
-
 def determine_winner(players):
     best_score = 0
     for player in players:
@@ -125,38 +105,98 @@ def determine_winner(players):
 
     winner = []
     for player in players:
-        if players[player] == score:
+        if total_value(players[player]) == best_score:
             winner.append(player)
+    
+    display_full_hands(players)
 
     if len(winner) > 1:
-        return f'Tie: {", ".join(winner)}'
+        prompt(f'Tie: {", ".join(winner)}')
     elif len(winner) == 0:
-        return f'Dealer Wins'
+        prompt(f'Dealer Wins')
     else:
-        return winner[0]
+        prompt(f'{winner[0]} Wins')
 
 """
 Define game loops here
+
+For player:
+1. Ask player to hit or stay
+2. If stay stop asking
+3. If hit, show total value of cards, your hand, and check:
+    a. Did I bust?
+    b. Did I get 21?
+    c. If neither, repeat at step 1
+
+For computer:
+1. Always hit if total value is less than 17
+2. Hit until value between 17-21 or bust
 """
 
-def player_turn():
+def player_turn(players):
     
-    cards = players["player"]
+    cards = players['Player']
     
     while True:
         display_hands(players)
-        prompt('Hit or Stay?')
-        decision = input().lower()
         
-        if decision == 'stay':
-            break
-        elif decision == 'hit':
-            hit('player', deck)
+        if not twenty_one(cards):
+            prompt('Hit or Stay?')
+            decision = input().lower()
             
-        prompt(f'Total Value Player: {total_value(cards)}')
-        
+            if decision == 'stay':
+                break
+            elif decision == 'hit':
+                hit('Player', deck)
+            
         if bust(cards):
-            
+            prompt("You've busted!")
             break
         elif twenty_one(cards):
+            prompt("You got 21!")
             break
+
+def computer_turn(players):
+    
+    # Change this to take into account multiple players later
+    if bust(players['Player']):
+        return
+        
+    cards = players['Dealer']
+
+    while total_value(cards) < 17:
+        hit('Dealer', deck)
+
+        if bust(cards):
+            prompt("Dealer busted!")
+            break
+        elif twenty_one(cards):
+            prompt("Dealer got 21!")
+            break
+
+def play_game():
+    deal(players, deck)
+    player_turn(players)
+    computer_turn(players)
+    determine_winner(players)
+
+# Initialize the deck here:
+suites = ['D', 'H', 'C', 'S']
+cards = [num for num in range(2, 11)] + ['J', 'Q', 'K', 'A']
+HIDDEN_CARD = ('X', 'X')
+
+deck = list()
+for suite in suites:
+    for card in cards:
+        deck.append(tuple((suite, card)))
+
+# Initialize hands here: 
+# Can turn players into objects later that way we can initialize multiple 
+players = {
+    'Dealer': [],
+    'Player': []
+}
+
+# current_player = 'Player'
+
+play_game()
